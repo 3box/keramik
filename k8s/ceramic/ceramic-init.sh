59ef33212394cf09002b6ab7aff117fc6d97eba2
Mohsin Zaidi <2236875+smrz2001@users.noreply.github.com>
2023-05-12 10:34:22 -0400

    test: cas

--- k8s/ceramic/ceramic-init.sh
+++ k8s/ceramic/ceramic-init.sh
@@ -5,3 +5,4 @@ set -eo pipefail
 export CERAMIC_ADMIN_DID=$(composedb did:from-private-key ${CERAMIC_ADMIN_PRIVATE_KEY})
 
 CERAMIC_ADMIN_DID=$CERAMIC_ADMIN_DID envsubst < /ceramic-init/daemon-config.json > /config/daemon-config.json
+CERAMIC_ADMIN_PRIVATE_KEY=$CERAMIC_ADMIN_PRIVATE_KEY envsubst < /ceramic-init/daemon-config.json > /config/daemon-config.json

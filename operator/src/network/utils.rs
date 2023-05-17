41a1df033c1c8572c23e528e28819069bc3e3d8e
Nathaniel Cook <nvcook42@gmail.com>
2023-05-16 20:41:16 -0600

    fix: add tests for operator
    These tests follow a basic pattern. First the controller is basically
    responsible for making API calls to the k8s API. So to test it we
    use a combination of mocks, stubs and expect_test to build a basic
    test framework. The mock/stubs capture the requests made to the API
    and return responses. The `expect_test` crate is used to assert the
    correctness of the requests while making it easy to update the tests
    when the code itself is changed. Additionally an `ExpectPatch` concept
    is added that allows for  patching the default stubs instead of
    reproducing their entire contents for each test.

--- operator/src/network/utils.rs
+++ operator/src/network/utils.rs
@@ -1,7 +1,9 @@
 use anyhow::{anyhow, bail, Result};
-use keramik_common::peer_info::{PeerId, PeerIdx};
+use async_trait::async_trait;
+use keramik_common::peer_info::{PeerIdx, PeerInfo};
 use multiaddr::{Multiaddr, Protocol};
 use multihash::Multihash;
+use unimock::unimock;
 
 use crate::network::controller::{
     CERAMIC_SERVICE_NAME, CERAMIC_SERVICE_RPC_PORT, CERAMIC_STATEFUL_SET_NAME,
@@ -13,59 +15,76 @@ struct ErrorResponse {
     message: String,
 }
 
-pub fn peer_rpc_addr(ns: &str, peer: PeerIdx) -> String {
+/// Define the behavior we consume from the IPFS RPC API.
+#[unimock(api=RpcClientMock)]
+#[async_trait]
+pub trait RpcClient {
+    async fn peer_info(&self, ns: &str, peer: PeerIdx) -> Result<PeerInfo>;
+}
+
+pub struct HttpRpcClient;
+
+fn peer_rpc_addr(ns: &str, peer: PeerIdx) -> String {
     format!("http://{CERAMIC_STATEFUL_SET_NAME}-{peer}.{CERAMIC_SERVICE_NAME}.{ns}.svc.cluster.local:{CERAMIC_SERVICE_RPC_PORT}")
 }
 
-pub async fn peer_addr(ns: &str, peer: PeerIdx) -> Result<(PeerId, String, Vec<String>)> {
-    let rpc_addr = peer_rpc_addr(ns, peer);
-    let client = reqwest::Client::new();
-    let resp = client
-        .post(format!("{}/api/v0/id", rpc_addr))
-        .send()
-        .await?;
-    if !resp.status().is_success() {
-        let data: ErrorResponse = resp.json().await?;
-        bail!("peer id failed: {}", data.message)
-    }
+#[async_trait]
+impl RpcClient for HttpRpcClient {
+    async fn peer_info(&self, ns: &str, index: PeerIdx) -> Result<PeerInfo> {
+        let rpc_addr = peer_rpc_addr(ns, index);
+        let client = reqwest::Client::new();
+        let resp = client
+            .post(format!("{}/api/v0/id", rpc_addr))
+            .send()
+            .await?;
+        if !resp.status().is_success() {
+            let data: ErrorResponse = resp.json().await?;
+            bail!("peer id failed: {}", data.message)
+        }
 
-    #[derive(serde::Deserialize)]
-    struct Response {
-        #[serde(rename = "ID")]
-        id: String,
-        #[serde(rename = "Addresses")]
-        addresses: Vec<String>,
-    }
-    let data: Response = resp.json().await?;
+        #[derive(serde::Deserialize)]
+        struct Response {
+            #[serde(rename = "ID")]
+            id: String,
+            #[serde(rename = "Addresses")]
+            addresses: Vec<String>,
+        }
+        let data: Response = resp.json().await?;
 
-    let p2p_proto = Protocol::P2p(Multihash::from_bytes(
-        &multibase::Base::Base58Btc.decode(data.id.clone())?,
-    )?);
-    // We expect to find at least one non loop back address
-    let addrs = data
-        .addresses
-        .iter()
-        .map(|addr| -> Multiaddr { addr.parse().expect("should be a valid multiaddr") })
-        .filter(|addr| {
-            // Address must have both a non loopback ip4 address and a tcp or quic endpoint
-            addr.iter().any(|proto| match proto {
-                multiaddr::Protocol::Ip4(ip4) => !ip4.is_loopback(),
-                _ => false,
+        let p2p_proto = Protocol::P2p(Multihash::from_bytes(
+            &multibase::Base::Base58Btc.decode(data.id.clone())?,
+        )?);
+        // We expect to find at least one non loop back address
+        let p2p_addrs = data
+            .addresses
+            .iter()
+            .map(|addr| -> Multiaddr { addr.parse().expect("should be a valid multiaddr") })
+            .filter(|addr| {
+                // Address must have both a non loopback ip4 address and a tcp or quic endpoint
+                addr.iter().any(|proto| match proto {
+                    multiaddr::Protocol::Ip4(ip4) => !ip4.is_loopback(),
+                    _ => false,
+                })
             })
-        })
-        // Add peer id to multiaddrs
-        .map(|mut addr| {
-            addr.push(p2p_proto.clone());
-            addr.to_string()
-        })
-        .collect::<Vec<String>>();
+            // Add peer id to multiaddrs
+            .map(|mut addr| {
+                addr.push(p2p_proto.clone());
+                addr.to_string()
+            })
+            .collect::<Vec<String>>();
 
-    if !addrs.is_empty() {
-        Ok((data.id, rpc_addr, addrs))
-    } else {
-        Err(anyhow!(
-            "peer {} does not have any valid non loopback addresses",
-            peer
-        ))
+        if !p2p_addrs.is_empty() {
+            Ok(PeerInfo {
+                index,
+                peer_id: data.id,
+                rpc_addr,
+                p2p_addrs,
+            })
+        } else {
+            Err(anyhow!(
+                "peer {} does not have any valid non loopback addresses",
+                index
+            ))
+        }
     }
 }

2601e1c718d2f81f6814ca503c6ac403af5a05e4
Mohsin Zaidi <2236875+smrz2001@users.noreply.github.com>
2023-06-09 10:04:13 -0400

    test: enable cas auth

--- operator/src/network/ceramic.rs
+++ operator/src/network/ceramic.rs
@@ -41,6 +41,7 @@ set -eo pipefail
 export CERAMIC_ADMIN_DID=$(composedb did:from-private-key ${CERAMIC_ADMIN_PRIVATE_KEY})
 
 CERAMIC_ADMIN_DID=$CERAMIC_ADMIN_DID envsubst < /ceramic-init/daemon-config.json > /config/daemon-config.json
+CERAMIC_ADMIN_PRIVATE_KEY=$CERAMIC_ADMIN_PRIVATE_KEY envsubst < /ceramic-init/daemon-config.json > /config/daemon-config.json
 "#.to_owned()),
 
 ("daemon-config.json".to_owned(),
@@ -70,7 +71,9 @@ r#"{
     "network": {
         "name": "dev-unstable"
     },
-    "node": {},
+    "node": {
+        "privateSeedUrl": "inplace:ed25519#${CERAMIC_ADMIN_PRIVATE_KEY}"
+    },
     "state-store": {
         "mode": "fs",
         "local-directory": "${CERAMIC_STATE_STORE_PATH}"

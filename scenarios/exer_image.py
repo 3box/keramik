71356a52f9f1aa281c50e1064ec37d5933ff9297
Golda Velez <golda@3box.io>
2023-08-19 00:12:44 -0700

    fix: avoid divide-by-zero errors on peers

--- scenarios/exer_image.py
+++ scenarios/exer_image.py
@@ -12,7 +12,7 @@ except:
   print("Choose an available tag from https://hub.docker.com/r/ceramicnetwork/js-ceramic/tags")
   exit(0)
 
-label = 'load-with-delay-for-' + img_tag
+label = 'load-with-network-delay-for-' + img_tag
 
 # replace for valid chars only
 label = re.sub(r'\.', '-', label)
@@ -27,6 +27,9 @@ os.system("perl -pi -e 's/^  name:.*$/  name: {}/g' network-with-cas.yaml".forma
 
 # apply the label to the simulation
 os.system("perl -pi -e 's/^  namespace:.*$/  namespace: keramik-{}/g' write-only.yaml".format(label))
+
+# apply the label to the delay config
+os.system("perl -pi -e 's/cas\..*\.svc\.cluster\.local/cas.keramik-{}.svc.cluster.local/g' delay-cas.yaml".format(label))
 
 # create the network
 os.system('kubectl apply -f network-with-cas.yaml')
@@ -57,10 +60,34 @@ os.system('kubectl label namespace keramik-{} istio-injection=enabled'.format(la
 
 os.system('kubectl apply -f delay-cas.yaml')
 
+# restart the pods to make sure the delays are applied
+os.system('kubectl delete pod ceramic-0 -n keramik-{}'.format(label))
+os.system('kubectl delete pod cas-0 -n keramik-{}'.format(label))
+
 # sleep after pods start to avoid initialization issues
 sleep(30)
 
 os.system('kubectl apply -f write-only.yaml')
+
+sleep(10)
+
+# check for errors, restart if needed
+def get_error_pod():
+    command = "kc get pods | grep 'simulate-manager' | grep 'Error' | awk '{print $1}'"
+    pod_name = subprocess.check_output(command, shell=True).decode('utf-8').strip()
+    return pod_name
+
+err_pod = get_error_pod()
+if err_pod:
+    print("Restarting simulation, error")
+    os.system('kubectl delete -f write-only.yaml')
+    sleep(10)
+    os.system('kubectl apply -f write-only.yaml')
+    sleep(10)
+    err_pod = get_error_pod()
+    if err_pod:
+        print("Too many errors, Terminating run for " + label)
+        exit(0)
 
 print("Running simulation for " + label)
 
